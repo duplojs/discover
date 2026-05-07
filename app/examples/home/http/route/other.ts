3cf8826046f27018cc2b15b6f88a9c8269be96be
mathcovax <campani.mathieu@gmail.com>
2026-05-07 12:26:25 +0000

    feat: update all package

--- app/examples/home/http/route/other.ts
+++ app/examples/home/http/route/other.ts
@@ -5,8 +5,12 @@ declare function createPost(userId: any, post: any): Promise<void>;
 // ---cut-end---
 const app = express();
 
-app.get("/users/:userId/posts", express.json(), async(req, res) => {
-	await createPost(req.params.userId, req.body);
+app.get(
+	"/users/:userId/posts",
+	express.json(),
+	async(req, res) => {
+		await createPost(req.params.userId, req.body);
 
-	res.status(204).send("Hello World");
-});
+		res.status(204).send("Hello World");
+	},
+);
